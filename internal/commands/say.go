af03a3aa91b96f0f01807a96fcb45016d2fba257
Jeremiah Payne <jeremiahp@redmondinc.com>
2022-09-30 11:41:28 -0600

    Added help for say command.

--- internal/commands/say.go
+++ internal/commands/say.go
@@ -4,6 +4,14 @@ import (
 	"fmt"
 )
 
+func (h BotCommandHelp) Say(request BotCommand) (response HelpResponse) {
+    response.Help = "Have Bender say something in a specified channel."
+
+    response.Description = "Cause the bot to say something. Usage: '!say in {channel} {text}'"
+
+    return response
+}
+
 func (bc BotCommand) Say(event BotCommand) (response Response, err error) {
 	response.Type = "command"
 	response.Message = fmt.Sprintf(`/echo "%s" 1`, event.body)
